95c0ba9b58be1f6831e214ab768b57cc14c58218
jeddelan <johan.eddeland@volvocars.com>
2020-06-16 16:29:05 +0200

    Add parameters for artificial specs

--- initializeReqParameters.m
+++ initializeReqParameters.m
@@ -259,6 +259,122 @@ base_cfg.ARCH_AT6c_req.ARCH_AT6c_speedEndTime = 20;
 
 base_cfg.ARCH_AT6c_act = base_cfg.ARCH_AT6c_req;
 
+%% Artificial thresholds
+% The following are thresholds used for the "artificial" inptus added in
+% "AT_and_specifications_artificial". 
+% Each input has the range [0 100], so setting a threshold to 10 will make
+% only 10% of falsified cases falsify also the artificial requirement. 
+
+% Example:
+% ADA_req has two "artificial" constants connected to it. The requirement
+% ADA_req_artificial is 
+% (ADA_req) or 
+%   (artificial_ADA <= artificial_ADA_constant) or
+%   (artificial_ASL_ADA <= artificial_ASL_ADA_constant)
+% To falsify this, if both constants are 10, ADA_req_artificial should only
+% be falsified for around 1% of the cases where ADA_req is falsified. 
+
+% Copy all non-artifical specs into their corresponding artificial ones
+baseFieldNames = fieldnames(base_cfg);
+for specCounter = 1:numel(baseFieldNames)
+    thisField = baseFieldNames{specCounter};
+    if contains(thisField, '_artificial')
+        % Skip if artificial
+        continue
+    end
+    base_cfg.([thisField '_artificial']) = base_cfg.(thisField);
+end
+
+% Now, add artificial-only parameters
+base_cfg.ADA_req_artificial.artificial_ADA_constant = 30;
+base_cfg.ADI_req_artificial.artificial_ADI_constant = 30;
+base_cfg.ADI_req_artificial.artificial_ADI_BTL_constant = 30;
+base_cfg.AFE_req_artificial.artificial_AFE_RFC_constant = 30;
+base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant = 30;
+base_cfg.AOT_req_artificial.artificial_AOT_ASL_constant = 30;
+base_cfg.ASL_req_artificial.artificial_ASL_ADA_constant = 30;
+
+% Copy values to all the other _req requirements where needed
+base_cfg.BTL_req_artificial.artificial_ADI_BTL_constant = ...
+    base_cfg.ADI_req_artificial.artificial_ADI_BTL_constant;
+base_cfg.RFC_req_artificial.artificial_AFE_RFC_constant = ...
+    base_cfg.AFE_req_artificial.artificial_AFE_RFC_constant;
+base_cfg.AFE_req_artificial.artificial_AOT_AFE_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant;
+base_cfg.AFE_req_artificial.artificial_AOT_AFE_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant;
+base_cfg.ASL_req_artificial.artificial_AOT_ASL_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_ASL_constant;
+base_cfg.ADA_req_artificial.artificial_ASL_ADA_constant = ...
+    base_cfg.ASL_req_artificial.artificial_ASL_ADA_constant;
+
+% Copy values to all _act requirements
+base_cfg.ADA_act.artificial_ADA_constant = ...
+    base_cfg.ADA_req_artificial.artificial_ADA_constant;
+base_cfg.ADI_act.artificial_ADI_constant = ...
+    base_cfg.ADI_req_artificial.artificial_ADI_constant;
+base_cfg.ADI_act.artificial_ADI_BTL_constant = ...
+    base_cfg.ADI_req_artificial.artificial_ADI_BTL_constant;
+base_cfg.AFE_act.artificial_AFE_RFC_constant = ...
+    base_cfg.AFE_req_artificial.artificial_AFE_RFC_constant;
+base_cfg.AOT_act.artificial_AOT_AFE_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant;
+base_cfg.AOT_act.artificial_AOT_ASL_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_ASL_constant;
+base_cfg.ASL_act.artificial_ASL_ADA_constant = ...
+    base_cfg.ASL_req_artificial.artificial_ASL_ADA_constant;
+base_cfg.BTL_act.artificial_ADI_BTL_constant = ...
+    base_cfg.ADI_req_artificial.artificial_ADI_BTL_constant;
+base_cfg.RFC_act.artificial_AFE_RFC_constant = ...
+    base_cfg.AFE_req_artificial.artificial_AFE_RFC_constant;
+base_cfg.AFE_act.artificial_AOT_AFE_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant;
+base_cfg.AFE_act.artificial_AOT_AFE_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_AFE_constant;
+base_cfg.ASL_act.artificial_AOT_ASL_constant = ...
+    base_cfg.AOT_req_artificial.artificial_AOT_ASL_constant;
+base_cfg.ADA_act.artificial_ASL_ADA_constant = ...
+    base_cfg.ASL_req_artificial.artificial_ASL_ADA_constant;
+
+% Artificial thresholds for ARCH specs
+base_cfg.ARCH_AT1_req_artificial.artificial_ARCH_AT1_constant = 30;
+base_cfg.ARCH_AT2_req_artificial.artificial_ARCH_AT2_constant = 30;
+base_cfg.ARCH_AT51_req_artificial.artificial_ARCH_AT5_constant = 30;
+base_cfg.ARCH_AT6a_req_artificial.artificial_ARCH_AT6_constant = 30;
+
+% Copy values to other ARCH _req specs as needed
+base_cfg.ARCH_AT52_req_artificial.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT51_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT53_req_artificial.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT51_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT54_req_artificial.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT51_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT6b_req_artificial.artificial_ARCH_AT6_constant = ...
+    base_cfg.ARCH_AT6a_req_artificial.artificial_ARCH_AT6_constant;
+base_cfg.ARCH_AT6c_req_artificial.artificial_ARCH_AT6_constant = ...
+    base_cfg.ARCH_AT6a_req_artificial.artificial_ARCH_AT6_constant;
+
+% Copy values to all ARCH _act specs
+base_cfg.ARCH_AT1_act.artificial_ARCH_AT1_constant = ...
+    base_cfg.ARCH_AT1_req_artificial.artificial_ARCH_AT1_constant;
+base_cfg.ARCH_AT2_act.artificial_ARCH_AT2_constant = ...
+    base_cfg.ARCH_AT2_req_artificial.artificial_ARCH_AT2_constant;
+base_cfg.ARCH_AT51_act.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT51_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT52_act.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT52_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT53_act.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT53_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT54_act.artificial_ARCH_AT5_constant = ...
+    base_cfg.ARCH_AT54_req_artificial.artificial_ARCH_AT5_constant;
+base_cfg.ARCH_AT6a_act.artificial_ARCH_AT6_constant = ...
+    base_cfg.ARCH_AT6a_req_artificial.artificial_ARCH_AT6_constant;
+base_cfg.ARCH_AT6b_act.artificial_ARCH_AT6_constant = ...
+    base_cfg.ARCH_AT6b_req_artificial.artificial_ARCH_AT6_constant;
+base_cfg.ARCH_AT6c_act.artificial_ARCH_AT6_constant = ...
+    base_cfg.ARCH_AT6c_req_artificial.artificial_ARCH_AT6_constant;
+
+
 %% params struct with everything
 all_specs = fieldnames(base_cfg);
 for ispec = 1:numel(all_specs)

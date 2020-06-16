7e98e367307ab678972ee2e80c9e5636078793a7
jeddelan <johan.eddeland@volvocars.com>
2020-06-16 16:36:59 +0200

    Add path to artificial folders in init file

--- init_ATwSS.m
+++ init_ATwSS.m
@@ -4,7 +4,9 @@ InitBreach;
 addpath('./specTransformer/');
 addpath('./src');
 addpath('./specRefModels');
+addpath('./specRefModelsArtificial');
 bdclose('AT_and_specifications_breach');
+bdclose('AT_and_specifications_artificial_breach');
 
 %% init instances
 initializeReqParameters;

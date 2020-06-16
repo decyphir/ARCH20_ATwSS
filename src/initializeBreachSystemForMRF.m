c4b7e08a75533f9590d7a40ffd8c77dca12aa567
jeddelan <johan.eddeland@volvocars.com>
2020-06-16 16:32:44 +0200

    Add specificsetup for artificial system

--- src/initializeBreachSystemForMRF.m
+++ src/initializeBreachSystemForMRF.m
@@ -6,10 +6,18 @@ else
     B = BreachSimulinkSystem(model, params);
 end
 
-nCP = [7 3];
-input_gen.type = 'UniStep';   % uniform time steps
-input_gen.cp = nCP;           % number of control points
-input_gen.method = {'pchip', 'pchip'};
+if contains(model, '_artificial')
+    nCP = [7 3 ones(1, 11)];
+    input_gen.type = 'UniStep';   % uniform time steps
+    input_gen.cp = nCP;           % number of control points
+    input_gen.method = repmat({'pchip'}, 1, 13);
+else
+    nCP = [7 3];
+    input_gen.type = 'UniStep';   % uniform time steps
+    input_gen.cp = nCP;           % number of control points
+    input_gen.method = {'pchip', 'pchip'};
+end
+
 B.SetInputGen(input_gen);
 for k = 1:nCP(1)
     % Set range of accelerator to [0 100]
@@ -19,6 +27,17 @@ end
 for k = 1:nCP(2)
     % Set range of brake to [0 325]
     eval(['B.SetParamRanges({''brake_u' num2str(k-1) '''}, [0 325]);']);
+end
+
+if contains(model, '_artificial')
+    % Set all input signals with "artificial" in the name to have range 
+    % [0 100]
+    allSigs = B.GetSignalList;
+    artificialSigs = allSigs(contains(allSigs, 'artificial_'));
+    for sigCounter = 1:numel(artificialSigs)
+        thisArtSig = artificialSigs{sigCounter};
+        eval(['B.SetParamRanges({''' thisArtSig '_u0''}, [0 100]);']);
+    end
 end
 
 % Setup disk caching
